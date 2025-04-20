7f243581f01dadc007faddcb5b37b043112ca7bd
unknown <marcosguattavo2@gmail.com>
2025-04-20 16:23:03 -0300

    Questão 12

--- lista 1/12.py
+++ lista 1/12.py
@@ -1,1 +1,18 @@
-#hhdhdhdhdhd
+#Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
+
+#A) Para homens: (72.7*h) - 58 
+
+h = float(input("Digite sua aultura; "))
+
+p1 = (72.7*h) - 58 
+r = round(p1,2)
+
+print("O peso ideal masculina é",r)
+
+#B) Para mulheres: (62.1*h) - 44.7 
+
+p2 = (62.1*h) - 44.7 
+
+r2 = round(p2,2)
+
+print("O peso ideal feminino é",r2)
