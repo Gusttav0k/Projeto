4acc37364ac1292bf8ce083a75113b19c59bf210
unknown <marcosguattavo2@gmail.com>
2025-04-22 18:26:12 -0300

    Questão 2/3

---
+++ lista 2/3.py
@@ -0,0 +1,8 @@
+#Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo
+
+a = float(input("Digite um numero: "))
+
+if a < 0:
+    print("O numero e negativo")
+else:
+    print("O numero e positivo ")
