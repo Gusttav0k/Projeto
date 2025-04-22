9f999bf8e3c1b26953f63845d782e4c237875bf4
unknown <marcosguattavo2@gmail.com>
2025-04-22 18:17:47 -0300

    Questão 2/2

---
+++ lista 2/2.py
@@ -0,0 +1,9 @@
+#Faça um Programa que peça dois números e imprima o maior deles.
+
+a = int(input('Digite um numeo: '))
+b = int(input('Digite outro numeo: '))
+
+if a > b :
+    print("O numero maior é o",a)
+else:
+    print("O numero maior é o",b)
