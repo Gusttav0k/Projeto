26c5bc27dab365860e484021ade50f94107acd79
unknown <marcosguattavo2@gmail.com>
2025-04-22 17:43:41 -0300

    Questão 2/1

--- lista 2/1.py
+++ lista 2/1.py
@@ -1,3 +1,5 @@
+#Faça um programa que peça o tamanho de um arquivo para download (em MB) e a velocidade de um link de Internet (em Mbps), calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos). 
+
 m = int(input("Digite o tamanho do arquivo em MB:"))
 v = float(input("Agora fala a velocidade da internet em mbps:"))
 
