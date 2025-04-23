a1c4095db604637a6c0db3240b226a973872ccab
unknown <marcosguattavo2@gmail.com>
2025-04-23 00:18:14 -0300

    Questão 2/11

---
+++ lista 2/8.py
@@ -0,0 +1,13 @@
+#Faça um Programa que leia três números e mostre o maior e o menor deles.
+
+a = float(input("Digite um numero: "))
+b = float(input("Digite um numero: "))
+c = float(input("Digite um numero: "))
+
+maior = max(a, b, c,)
+
+menor = min(a, b, c,)
+
+print("O maior numero é",maior)
+print("O mmenor numero é",menor)
+
