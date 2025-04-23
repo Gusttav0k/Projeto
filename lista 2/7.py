19a413dac6167c347a03e25e2cd180355230f36e
unknown <marcosguattavo2@gmail.com>
2025-04-23 00:14:46 -0300

    Questão 2/7

---
+++ lista 2/7.py
@@ -0,0 +1,9 @@
+#Faça um Programa que leia três números e mostre o maior deles. 
+
+a = float(input("Digite um numero: "))
+b = float(input("Digite um numero: "))
+c = float(input("Digite um numero: "))
+
+maior = max(a, b, c,)
+
+print("O maior numero é",maior)
